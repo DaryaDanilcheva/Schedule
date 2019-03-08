153e1ecc5924f022dab7328fba8e2fff680cfe6c
DaryaDanilcheva <39412849+DaryaDanilcheva@users.noreply.github.com>
2019-03-09 00:37:12 +0400

    Add files via upload

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -12,7 +12,15 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            List<string> weeks = Parse();
+            string classesTable = ParseClasses();
+            List<string> classes = MySplit(classesTable, new string[] { "</TD>" });
+
+            List<string> groups = FillGroups(classes);
+            List<string> hrefs = FillHref(classes);
+
+            Console.WriteLine(groups[5] +" "+hrefs[5]);
+
+            List<string> weeks = Parse(hrefs[5]);
 
             Week week1 = new Week();
             Week week2 = new Week();
@@ -26,10 +34,68 @@ namespace ConsoleApp1
             week1 = FillWeek(week1, lessonOfWeek1);
             week2 = FillWeek(week2, lessonOfWeek2);
 
+            Console.WriteLine("Week1");
             ShowSchedule(week1);
+            Console.WriteLine("Week2");
             ShowSchedule(week2);
 
             Console.ReadKey();
+        }
+
+        static List<string> FillHref(List<string> classes)
+        {
+            List<string> hrefs = new List<string>();
+            for (int i = 0; i < classes.Count(); i++)
+            {
+                if (classes[i].Contains("HREF"))
+                {
+                    int start = classes[i].IndexOf("<A HREF=") + 9;
+                    int end = classes[i].IndexOf('>', start) - 1;
+                    string href = classes[i].Substring(start, end - start);
+                    hrefs.Add(href);
+                }
+            }
+            return hrefs;
+        }
+
+        static List<string> FillGroups(List<string> classes)
+        {
+            List<string> groups = new List<string>();
+            for (int i = 0; i < classes.Count(); i++)
+            {
+                if (classes[i].Contains("HREF"))
+                {
+                    string group = classes[i];
+                    bool skobka = true;
+                    while (skobka)
+                    {
+                        int start1 = group.IndexOf("<");
+                        if (start1 >= 0)
+                        {
+                            int end1 = group.IndexOf(">", start1);
+                            if (end1 > 0)
+                            {
+                                group = group.Remove(start1, end1 + 1 - start1);
+                            }
+                        } else
+                        {
+                            skobka = false;
+                        }
+                    }
+                    groups.Add(group.Trim());
+                }
+            }
+            return groups;
+        }
+
+        static string ParseClasses()
+        {
+            WebClient webClient = new WebClient();
+            string response = webClient.DownloadString("http://www.ulstu.ru/schedule/students/part1/raspisan.htm");
+            int start = response.IndexOf("<TABLE");
+            int end = response.IndexOf("</TABLE>");
+            string classesTable = response.Substring(start, end - start);
+            return classesTable;
         }
 
         static Week FillWeek(Week week, List<string> lessonOfWeek)
@@ -61,12 +127,12 @@ namespace ConsoleApp1
             }
         }
 
-        static List<string> Parse()
+        static List<string> Parse(string href)
         {
             List<string> weeks = new List<string>();
 
             WebClient webClient = new WebClient();
-            string response = webClient.DownloadString("http://www.ulstu.ru/schedule/students/part1/55.htm");
+            string response = webClient.DownloadString("http://www.ulstu.ru/schedule/students/part1/"+href);
             int start = response.IndexOf("<TABLE");
             int end = response.IndexOf("</TABLE>");
             string week1 = response.Substring(start, end - start);
